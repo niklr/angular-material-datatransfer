cbef707a8d65cd84b09037447f3908349c8539f3
niklr <r.niklaus@gmx.ch>
2017-05-16 14:48:12 +0200

    flex-layout prototype added

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -3,4 +3,47 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ApiService {
   title = 'Angular Datatransfer';
+
+  testItems = [
+    {
+      'name': 'DICOM_patientXY_1.dcm',
+      'size': '15 MB',
+      'transferType': 'Upload',
+      'status': 'Uploading',
+      'progress': 50,
+      'speed': '489.7 KB/s',
+      'elapsedTime': '00:00:49',
+      'remainingTime': '00:02:00'
+    },
+    {
+      'name': 'DICOM_patientXY_2.dcm',
+      'size': '17 MB',
+      'transferType': 'Upload',
+      'status': 'Failed',
+      'progress': 0,
+      'speed': '',
+      'elapsedTime': '',
+      'remainingTime': ''
+    },
+    {
+      'name': 'SMIR.Thorax.089Y.M.CT.7.000.dcm.zip',
+      'size': '2 GB',
+      'transferType': 'Download',
+      'status': 'Queued',
+      'progress': 0,
+      'speed': '',
+      'elapsedTime': '',
+      'remainingTime': ''
+    },
+    {
+      'name': 'NIFTI_patientXY.nii',
+      'size': '12 GB',
+      'transferType': 'Upload',
+      'status': 'Queued',
+      'progress': 0,
+      'speed': '',
+      'elapsedTime': '',
+      'remainingTime': ''
+    }
+  ];
 }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -14,3 +14,16 @@ if (process.env.ENV === 'build') {
 
   require('zone.js/dist/long-stack-trace-zone');
 }
+
+// https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
+if (!String.prototype.endsWith) {
+  String.prototype.endsWith = function(searchString, position) {
+      let subjectString = this.toString();
+      if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+        position = subjectString.length;
+      }
+      position -= searchString.length;
+      let lastIndex = subjectString.indexOf(searchString, position);
+      return lastIndex !== -1 && lastIndex === position;
+  };
+}
