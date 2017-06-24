d708fc4f3e05cc1afb1af82bff7af0d4f82a2943
niklr <r.niklaus@gmx.ch>
2017-06-24 13:34:33 +0200

    ribbon added

--- demo/download_generator.bat
+++ demo/download_generator.bat
@@ -2,7 +2,7 @@ mkdir download
 cd download
 fsutil file createnew sample_file_1.tmp 27078904
 fsutil file createnew sample_file_2.tmp 12754944
-fsutil file createnew sample_file_3.tmp 144360000
-fsutil file createnew sample_file_4.tmp 254710000
+fsutil file createnew sample_file_3.tmp 14436000
+fsutil file createnew sample_file_4.tmp 25471000
 cd ..
 xcopy "download" "..\src\public\files" /s/h/e/k/f/c
