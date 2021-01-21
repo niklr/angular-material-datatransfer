6e62c5380a52f09eabf60a40425a58fc1f54ee4e
ShawFung Chao <zhaoxiaochun@sailing3d.cn>
2021-01-21 17:31:05 +0800

    update angular 11

--- projects/amd-lib/src/lib/angular-material-datatransfer.component.spec.ts
+++ projects/amd-lib/src/lib/angular-material-datatransfer.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AngularMaterialDatatransferComponent } from './angular-material-datatransfer.component';
 
@@ -6,7 +6,7 @@ describe('AngularMaterialDatatransferComponent', () => {
   let component: AngularMaterialDatatransferComponent;
   let fixture: ComponentFixture<AngularMaterialDatatransferComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AngularMaterialDatatransferComponent ]
     })
